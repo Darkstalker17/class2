1c22b5763483f5b266204c373fd37baddffec28d
Darkstalker17 <dakshithkarthik.a@gmail.com>
2025-05-07 11:06:22 +0400

    Add files via upload

---
+++ class2_1.py
@@ -0,0 +1,38 @@
+
+'''
+1. Welcome to the world of coding
+Outline:
+Write a program for print command.
+'''
+
+print("Welcome to the world of programming")
+#print a number
+print(25)
+#next line after print
+print("Hi my name is Dakshith,\nI am 13 years old")
+print("Hello", 5)
+#End argument of print statement
+print("Welcome to",end=" !@#$%^")
+print()
+'''
+2. Understanding the variables
+Outline:
+Write a program to create different variables to store various values.
+'''
+#creating different types of variables
+x = 24
+print(x)
+y = "Dakshith"
+print(y)
+z = input("Enter your name : ")
+print("Hello", z, ", welcome to codingal")
+
+
+'''
+3. Keywords
+Outline:
+Write a program to print all the keywords
+'''
+import keyword
+print("The Python Keywords are: \n")
+print(keyword.kwlist)
